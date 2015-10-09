2892c2984f709dbf53ed7ed682d5b4a3a1e841e4
Ole Michaelis <Ole.Michaelis@googlemail.com>
2015-10-09 16:04:54 +0200

    working sse skeleton

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
 var express = require('express')
-var sse = require('sse-stream')
+var SSE = require('sse')
+var EventEmitter = require('events').EventEmitter
 var uuid = require('node-uuid')
+var bodyParser = require('body-parser')
+
 var app = express()
+var board_exchange = new EventEmitter()
 
 app.set('view engine', 'jade')
 
+app.use('/public', express.static('public'))
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.text()) // for parsing application/json
+
 app.get('/', function (req, res) {
   res.render('index', { hash: uuid.v4() })
+})
+
+app.get('/board/:id', function (req, res) {
+  console.log(`client (${req.params.id}) connected`)
+
+  var client = new SSE.Client(req, res)
+  client.initialize()
+
+  client.send('oh', 'hi')
+
+  board_exchange.on(req.params.id, function (board) {
+    client.send('new-board', JSON.stringify(board))
+  })
+
+  var interval = setInterval(function () {
+    client.send('heartbeat', '<3')
+  }, 10000)
+  client.on('close', function () {
+    console.log(`client (${req.params.id}) gone`)
+    clearInterval(interval)
+  })
 })
 
 app.get('/:id', function (req, res) {
   res.render('board', { id: req.params.id })
 })
 
-var Readable = require('stream').Readable
-function objectStream () {
-  var stream = new Readable({ objectMode: true })
-  var i = 0
-  stream._read = function () {
-    this.push({foo: ++i})
-    if (i === 300) this.push(null)
-  }
-  return stream
-}
-
-app.get('/board/:id', function (req, res) {
-  objectStream().pipe(sse()).pipe(res)
+app.post('/:id', function (req, res) {
+  board_exchange.emit(req.params.id, req.body)
+  res.end('LONG LIFE CONWAY')
 })
 
 var server = app.listen(process.env.PORT || 3000, function () {
